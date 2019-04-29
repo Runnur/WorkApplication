4efe161c7cd51403ca26db51f8e2a9bbde39fc7f
Denis Yurchenko <yurdenis@gmail.com>
2019-04-29 16:40:16 +0300

    ShippingDetails

---
+++ WorkApplication.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkApplication.Domain.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage = "Укажите ваше имя")]
+        public string Name { get; set; }
+    }
+}
