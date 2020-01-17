566f3c6f94a26ec0915e6d1a80d3e51bf60b9df8
Denis Yurchenko <yurdenis@gmail.com>
2020-01-17 17:47:43 +0300

    new

---
+++ WorkApplication.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using WorkApplication.Domain.Abstract;
+using WorkApplication.Domain.Entities;
+
+namespace WorkApplication.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = @"orders@example.com";
+        public string MailFromAddress = "gamestore@example.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = true;
+        public string FileLocation = @"c:\game_store_emails";
+    }
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings emailSettings;
+
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            emailSettings = settings;
+        }
+
+        public void ProcessOrder(Cart cart, ShippingDetails shippingDetails)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.ServerName;
+                smtpClient.Port = emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(emailSettings.Username, emailSettings.Password);
+
+                if (emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                    .AppendLine("Новый заказ обработан")
+                    .AppendLine("---")
+                    .AppendLine("Товары:");
+
+                foreach (var line in cart.Lines)
+                {
+                    var subtotal = line.Game.Price * line.Quantity;
+                    body.AppendFormat("{0} x {1} (итого: {2:c})", line.Quantity, line.Game.Name, subtotal);
+                }
+
+            }
+        }
+    }
+}
